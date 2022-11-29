f3f0036742aadceb65bc38c1437aefb9976a57d6
matiasrivero-ctrl <matiasariveros.cinco@gmail.com>
2022-11-29 10:55:03 -0300

    adding receive and fallback functions

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -7,24 +7,26 @@ pragma solidity ^0.8.0;
 
 import "./PriceConverter.sol";
 
+error NotOwner();
+
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minimumUSD = 50 * 1e18;
+    uint256 public constant MINIMUM_USD = 50 * 1e18;
 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
-    address public owner;
+    address public immutable i_owner;
 
     constructor() {
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     function fund() public payable{
         // Want to be able to set a minimum fund amount in USD
         //  1. How do we send ETH to this contract?
-        require(msg.value.getConversionRate() >= minimumUSD, "Didn't send enough!"); // 1e18 == 1 * 10 ** 18 == 1000000000000000000
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enough!"); // 1e18 == 1 * 10 ** 18 == 1000000000000000000
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
@@ -46,7 +48,16 @@ contract FundMe {
     }
 
     modifier onlyOwner  {
-        require(msg.sender == owner, "Sender is not owner!");
+        // require(msg.sender == i_owner, "Sender is not owner!");
+        if(msg.sender != i_owner) revert NotOwner();
         _;
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
     }
 }
